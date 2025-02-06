c5efbe29320e51ae9aff07060b0d81865424fcb3
OtavioParisDeV <otavioparis.dev@gmail.com>
2025-02-06 17:16:54 -0300

    tipos e declaracoes das variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
+import java.util.Date;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        final double VALOR_DE_PI = 3.14;
+
+//-Tipos de Variáveis e seus valores declarados
+
+        String nome = "Gabriel";
+        int idade = 24;
+        double peso = 104.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;   //-sim ou não
+        Date dataNascimento = new Date();
+
+        
+
+
+
     }
 }
