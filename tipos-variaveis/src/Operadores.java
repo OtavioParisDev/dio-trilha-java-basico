e61892ebcc0185e5c0d7225547d1593f1293d25b
OtavioParisDeV <otavioparis.dev@gmail.com>
2025-02-06 21:57:32 -0300

    encremento

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+
+    public static void main(String[] args) throws Exception {
+        
+        int numero = 10;
+
+        numero = -numero; // torna-lo negativo
+
+            System.out.println(numero);
+
+        numero = numero * -1; // torna-lo positivo
+
+            System.out.println(numero);
+        
+    }
+    
+}
